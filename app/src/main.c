249b5ce7f714454add6b41f0ee9bef2ff0f7cb13
TekCastPork <tekcastpork@gmail.com>
2025-10-16 19:17:44 -0600

    do the things (1)

--- app/src/main.c
+++ app/src/main.c
@@ -8,10 +8,22 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
+#define SW0NODE DT_ALIAS(sw0)
+
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0NODE, gpios);
+
 int main(void) {
+  printk("[Main]\tInit");
 
+  if(!gpio_is_ready_dt(&button)) return 0;
+
+  if(gpio_pin_configure_dt(&button, GPIO_INPUT)) return 0;
   while(1) {
-
+    if(gpio_pin_get_dt(&button)) {
+      printk("Button Poll Press");
+      while(gpio_pin_get_dt(&button)) k_msleep(10);
+    }
+    k_msleep(10);
   }
 	return 0;
 }
